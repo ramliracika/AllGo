b353b999c6e061e0ccbb2550c9ad978774fc3af0
Ramli Racika <ramliracika@gmail.com>
2021-09-08 20:08:24 +0700

    add multi insert

--- database/database_insert_test.go
+++ database/database_insert_test.go
@@ -20,3 +20,18 @@ func TestInsert(t *testing.T) {
 	fmt.Println("Succes Insert Data to Mahasiswa")
 
 }
+
+func TestMultiInsert(t *testing.T) {
+	db := GetConnet()
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(Id,Name,Address,Email,Phone,Birthday,Rating,isMarried) VALUES('ID2407','Azcareon','Mars','azcareon@xabre.com','0219876542','22-07-1998','5.0','false'),('ID1121','HayDim','Earth',Null,'0219876542','22-07-1998','4.0','true'),('ID3923','Razor','Pluto',Null,'0219876542',Null,'3.5','false');"
+
+	_, err := db.ExecContext(ctx, script) // to execute query sql without show data
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Succes Insert Data to Mahasiswa")
+
+}
